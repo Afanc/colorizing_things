bce7104cf140972becf7d2f6fd6c513b165c1c7f
cheshirecat12 <anthony.gillioz@outlook.com>
2019-04-18 14:49:27 +0200

    Rename the STL10 class to STL10GrayColor; Implementation of the STL10GrayColor class; Implementation of the convert function lab -> rgb

---
+++ src/STL10GrayColor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import numpy as np
+from torchvision.datasets import STL10
+from PIL import Image
+from skimage import io, color
+
+class STL10GrayColor(STL10):
+
+    def __getitem__(self, index):
+        """
+        Overwrite the original method to return a grayscaled image (L) and
+        the colors of the image (ab). The color space is CIE Lab.
+
+        Input:
+            index(int): index of the desired image
+
+        Return:
+            L_tensor(torch.Tensor): L dimension of the picture
+            ab_tensor(torch.Tensor): ab dimension of the picture
+        """
+        img = self.data[index]
+
+        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+            # Convert img RGB -> Lab
+            lab_img = color.rgb2lab(img)
+
+            # Transpose the lab img: dim 128x128x3 -> 3x128x128
+            L, a, b = np.transpose(lab_img, (2, 0, 1))
+
+            # Normalize
+            L_normalized = (L + 128) / 255
+            ab_normalized = np.asarray([a, b], dtype=np.float32)
+
+            # Transform numpy array to torch tensor
+            L_tensor = torch.from_numpy(L_normalized.astype(np.float32))
+            ab_tensor = torch.from_numpy(ab_normalized.astype(np.float32))
+
+        return (L_tensor, ab_tensor)
+
+if __name__ == "__main__":
+    # Example of usage:
+
+    # Image preprocessing
+    transform = transforms.Compose([transforms.Resize(128)])
+
+    # Load STL10 dataset
+    stl10_trainset = STL10GrayColor(root="./data",
+                                    split='train',
+                                    download=True,
+                                    transform=transform)
+
+    # Parameters
+    params_loader = {'batch_size': 32,
+                     'shuffle': False}
+
+    train_loader = DataLoader(stl10_trainset, **params_loader)
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,50 @@
 #!/usr/bin/python
-import STL10 as stl
+import warnings
+import numpy as np
+import torch
 import torchvision.transforms as transforms
-from torchvision.transforms import Resize, Grayscale
+from torchvision.transforms import Resize
+from skimage import color
+import STL10 as stl
 
 
-def get_dataset(size=128) : 
-
+def get_dataset(size=128):
     resize = transforms.Compose([Resize(size)])
-    
+
     all_images = stl.STL10(root='../data',
-                           split='train+unlabeled', 
+                           split='train+unlabeled',
                            transform=resize,
-                           target_transform=resize, 
+                           target_transform=resize,
                            download=True)
 
     return all_images
+
+
+def convert_lab2rgb(L, ab):
+    """Convert the batch images from lab -> rgb."""
+
+    # Normalize grayscale to rgb
+    L = L*255-128
+    # ab = ab*100
+
+    # Add missing dim:
+    # batch_size x img_size x img_size -> batch_size x 1 x img_size x img_size
+    L.unsqueeze_(1)
+
+    # Concatanet gray tensor with ab tensor
+    colorized_img = torch.cat((L, ab), 1)
+
+    # Change torch.Tensor to numpy array and permute the dimension
+    # bs x ch x h x w -> bs x h x w x ch
+    reversed_img = colorized_img.cpu().double().detach().permute(0, 2, 3, 1).numpy()
+
+    # Iterate over the images of the batch because the lab2rgb() function
+    # take in paramters only a tuple (h x w x ch).
+    for i in range(reversed_img.shape[0]):
+        # Remove warning of bad color range.
+        warnings.simplefilter("ignore")
+        reversed_img[i, :, :, :] = color.lab2rgb(reversed_img[i, :, :, :])
+
+    output = torch.from_numpy(reversed_img).float().permute(0, 3, 1, 2)
+
+    return output.cuda()
