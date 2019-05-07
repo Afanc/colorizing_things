dd3deded07cdff42e0f4968b5e77f348154409a4
cheshirecat12 <cheshirecat12@ext-131-86.eduroam.unibe.ch>
2019-05-07 15:38:43 +0200

    return to a static gan...

--- src/discriminator.py
+++ src/discriminator.py
@@ -58,3 +58,50 @@ class Discriminator(nn.Module):
         output = self.layers(input_)
 
         return output.view(-1, 1).squeeze(1)
+
+
+class StatiStaticcDiscriminator(nn.Module):
+    def __init__(self, ndf, ncc):
+        super(Discriminator, self).__init__()
+
+        self.layers = nn.Sequential(
+            # in_channels, out_channels, kernel_size, stride=1, padding=0,
+            # Formula: (H + 2*padding -kernel_size)/stride + 1
+            # (128 + 2*2 - 4)/2+1
+            # in: 3 x 128 x 128
+            # out: 128 x 65 x 65 (64 x 33 x 33)
+            nn.Conv2d(ncc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2),
+
+            #out: 256 x 33 x 33 (128 x 17 x 17)
+            nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2),
+
+            # out 512 x 17 x 17 (256 x 9 x 9)
+            nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2),
+
+            # out 1024 x 9 x 9 (512 x 5 x 5)
+            nn.Conv2d(ndf*4, ndf*8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2),
+
+            # # out: 1024 x 5 x 5
+            nn.Conv2d(ndf*8, ndf*8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2),
+
+            # out (1 x 1 x 1)
+            nn.Conv2d(ndf*8, 1, 4, 1, 0, bias=False),
+            # nn.Sigmoid()
+
+        )
+
+    def forward(self, input_):
+        # print("before")
+        output = self.layers(input_)
+        # print(output.shape)
+        # print("after")
+        return output.view(-1, 1).squeeze(1)
--- src/generator.py
+++ src/generator.py
@@ -73,3 +73,39 @@ class Generator(nn.Module):
         output = self.layers(input_)
 
         return output
+
+
+class StaticGenerator(nn.Module):
+    def __init__(self, nz=100, ngf=64, nc=3):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,  ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+
+        return output
